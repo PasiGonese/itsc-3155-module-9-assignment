5750fb2629a2749aa9185333b20e9e147817567c
pasi_gonese <pasigonese@gmail.com>
2022-10-25 10:48:25 -0400

    Testing features not yet complete though

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,18 @@
 # TODO: Feature 1
+import pytest
+from src.repositories.movie_repository import get_movie_repository
+from app import app
+
+#positive case
+def test_get_list_all_movies_correct():
+    new_movie = movie_repository.create_movie("Inception", "Christopher Nolan", 5)
+    assert "Inception" == new_movie.title
+    assert "Christopher Nolan" == new_movie.director
+    assert 5 == new_movie.rating
+
+#negative case
+def test_get_list_all_movies_correct():
+    new_movie = movie_repository.create_movie("Inception", "Christopher Nolan", 5)
+    assert "The Godfather" == new_movie.title
+    assert "Jordan Peele" == new_movie.director
+    assert 1 == new_movie.rating
