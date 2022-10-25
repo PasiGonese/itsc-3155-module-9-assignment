d7ab9491eb7570b5c6ed96f21811f0bc101d81b0
Ajay <ajayshankar65@yahoo.com>
2022-10-25 17:44:22 -0400

    unit test mods again

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,9 +1,20 @@
 # TODO: Feature 2
 import pytest
-from movie_repository import create_movie
+from src.repositories.movie_repository import get_movie_repository
 
-def test_create_movies():
-    movie = movie_repository.create_movie("Inception", "james", '1')
+movie_repository = get_movie_repository()
+
+# happy moment
+def test_create_movies_sunny_day():
+    movie = movie_repository.create_movie("Inception", "Christopher Nolan", '1')
     assert "Inception" == movie.title
-    assert "james" == movie.director
+    assert "Christopher Nolan" == movie.director
     assert "1" == movie.rating
+
+def test_create_movies_rainy_day():
+    movie1 = movie_repository.create_movie("Star Wars", "George Lucas", "10")
+    assert "Star Trek" != movie1.title
+    assert "James Cameron" != movie1.director
+    assert b'10' != movie1.rating
+
+    
