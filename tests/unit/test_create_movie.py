e61ac672d4194a9385f598d788e149b271cb0406
Ajay <ajayshankar65@yahoo.com>
2022-10-25 17:13:23 -0400

    unit test mod

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,9 +1,9 @@
 # TODO: Feature 2
 import pytest
-import movie_repository
+from movie_repository import create_movie
 
-def test_create_movies:
+def test_create_movies():
     movie = movie_repository.create_movie("Inception", "james", '1')
     assert "Inception" == movie.title
     assert "james" == movie.director
-    assert "1" = movie.rating
+    assert "1" == movie.rating
