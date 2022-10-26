f1a623d5166343799fb1f9379969fde50ba11647
cyron10 <82346848+cyron10@users.noreply.github.com>
2022-10-25 20:22:27 -0400

    Revert "Riley 10/25"

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 from turtle import title
->>>>>>> main
 from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
@@ -46,6 +43,4 @@ def create_movie():
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    x = request.args.get("search")
-    movie_repository.get_movie_by_title(x)
     return render_template('search_movies.html', search_active=True)
